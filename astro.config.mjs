2e042806f55c626b8d3ced9689518a0efd5568c2
Nick Mickley <nick.mickley@gmail.com>
2022-10-20 12:08:51 -0400

    set up navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import vue from '@astrojs/vue';
+import vue from '@astrojs/vue'
+import netlify from '@astrojs/netlify/functions'
 
 // https://github.com/vitejs/vite/tree/main/packages/plugin-vue
-import netlify from "@astrojs/netlify/functions";
-const vueConfig = {};
+const vueConfig = {}
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.firstavenuedecor.com',
   integrations: [vue(vueConfig)],
-  output: "server",
+  output: 'server',
+  trailingSlash: 'never',
   adapter: netlify()
-});
+})
---
+++ src/components/Navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <div class="container nav-container">
+      <ul class="level-1">
+        <li v-for="item1 in mainMenuItems">
+          <a :href="item1.url">{{ item1.title }}</a>
+
+          <ul v-if="item1.items?.length" class="level-2">
+            <li v-for="item2 in item1.items">
+              <a :href="item2.url">{{ item2.title }}</a>
+
+              <ul v-if="item2.items?.length" class="level-3">
+                <li v-for="item3 in item2.items">
+                  <a :href="item3.url">{{ item3.title }}</a>
+                </li>
+              </ul>
+
+            </li>
+          </ul>
+          
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { useStore } from '@nanostores/vue'
+import { menu, MENU } from '../store/menu'
+
+const $menu = useStore(menu)
+
+export default {
+  data() {
+    return {
+      mainMenuItems: $menu.value[MENU.MainMenu].items
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+	nav {
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+	}
+
+	.nav-container {
+
+	}
+</style>
