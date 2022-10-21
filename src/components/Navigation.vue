b59e2b6f0b2b68bffe1b971c79a7298691091869
Nick Mickley <nick.mickley@gmail.com>
2022-10-21 14:59:35 -0400

    Nav work

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,7 @@
     <div class="container nav-container">
       <ul class="level-1">
         <li v-for="item1 in mainMenuItems">
-          <a :href="item1.url">{{ item1.title }}</a>
+          <a :href="cleanUrl(item1.url)" class="level-1-link" :class="{ 'has-sub-items':  item1.items?.length }">{{ item1.title }}</a>
 
           <ul v-if="item1.items?.length" class="level-2">
             <li v-for="item2 in item1.items">
@@ -17,7 +17,7 @@
 
             </li>
           </ul>
-          
+
         </li>
       </ul>
     </div>
@@ -33,7 +33,14 @@ const $menu = useStore(menu)
 export default {
   data() {
     return {
-      mainMenuItems: $menu.value[MENU.MainMenu].items
+      mainMenuItems: $menu.value[MENU.MainMenu].items,
+      level2Show: {},
+    }
+  },
+
+  methods: {
+    cleanUrl(url: string): string {
+      return url.split('.com')[1]
     }
   }
 }
@@ -47,6 +54,58 @@ export default {
 	}
 
 	.nav-container {
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 4px 8px;
 	}
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    padding: 8px 8px;
+    position: relative;
+
+    &:hover > ul {
+      display: flex;
+    }
+  }
+
+  a {
+
+  }
+
+  .level-1 {
+    display: flex;
+    flex-wrap: nowrap;
+  }
+
+  .level-1-link {
+    &.has-sub-items:after {
+      content: ' ';
+      display: inline-block;
+      background-image: url(/icons/arrow-down.svg);
+      background-repeat: no-repeat;
+      background-size: contain;
+      width: 8px;
+      height: 8px;
+      margin-left: 5px;
+    }
+  }
+
+  .level-2 {
+    display: none;
+    flex-direction: column;
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -50%;
+    padding: 8px;
+    border: 1px solid var(--color-primary);
+  }
 </style>
