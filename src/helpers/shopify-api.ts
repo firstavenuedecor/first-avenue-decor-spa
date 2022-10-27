639a61117b70e66418b36a7fb16d7ade0f1be014
Nick Mickley <nick.mickley@gmail.com>
2022-10-27 11:24:03 -0400

    Env variable fix

--- src/helpers/shopify-api.ts
+++ src/helpers/shopify-api.ts
@@ -1,12 +1,17 @@
 import type { IApiResponse, API_PATH } from '../types'
 
 export async function shopifyApi<T>(path: API_PATH, ...queries: string[]): Promise<IApiResponse<T>> {
+  const storefrontAccessToken = import.meta.env.PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN
+  if (!storefrontAccessToken) {
+    throw new Error('Storefront Access Token environment variable not set.')
+  }
+
   const query = queries.length ? '?' + queries.filter(q => q).join('&') : ''
   const res = await fetch(`https://shopify-api-fad.herokuapp.com${path}${query}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'X-Shopify-Storefront-Access-Token': import.meta.env.PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+      'X-Shopify-Storefront-Access-Token': storefrontAccessToken,
     },
   })
   .then(res => res.json())
